16ec52a24d21f49c1f35f0d4e3f329101e238e4b
kiatAWDSA <lskiat@huskers.unl.edu>
2019-03-20 15:29:41 -0500

    Update license

--- libraries/TachoSensor/TachoSensor.cpp
+++ libraries/TachoSensor/TachoSensor.cpp
@@ -1,24 +1,3 @@
-/**********************************************************************
-Obtain tacho readings (e.g. a fan) and convert it to RPM.
-Utilizes the following chips:
-- 74HC590 (counts the pulses from tachometer)
-- 74HC165 (summarizes the counted pulses into a byte)
-
-Copyright 2018 Soon Kiat Lau
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-***********************************************************************/
-
 #include "TachoSensor.h"
 
 TachoSensor::TachoSensor(PCA9685& PCA9685Manager,
